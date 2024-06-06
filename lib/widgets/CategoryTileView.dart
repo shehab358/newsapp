47647c4c7d126cf15fc511ab984c47cb8bbdcd13
shehab358 <shehabyusuf12@gmail.com>
2024-06-06 23:45:25 +0300

    Finished Future Builder

--- lib/widgets/CategoryTileView.dart
+++ lib/widgets/CategoryTileView.dart
@@ -1,22 +1,35 @@
 // ignore: file_names
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:newsapp/services/news_services.dart';
 import 'package:newsapp/widgets/CategoryTileViewBuilder.dart';
 
 // ignore: must_be_immutable
-class CategoryTileView extends StatelessWidget {
-   CategoryTileView({super.key});
-
-  bool isLoading = true;
+class CategoryTileView extends StatefulWidget {
+  CategoryTileView({super.key});
 
   @override
+  State<CategoryTileView> createState() => _CategoryTileViewState();
+}
+
+class _CategoryTileViewState extends State<CategoryTileView> {
+  bool isLoading = true;
+  late Future future;
+  @override
+  void initState() {
+    future = NewsServices().getNews();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: NewsServices().getNews(),
+        future: future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return CategoryTileViewBuilder(articles: snapshot.data!);
+            return CategoryTileViewBuilder(articles: snapshot.data);
           } else if (snapshot.hasError) {
             return const Text("ERROR");
           } else {
