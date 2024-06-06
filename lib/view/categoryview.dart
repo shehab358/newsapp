c3841f02f85dafb1170abcc782f1b72bc0c9e4dd
shehab358 <shehabyusuf12@gmail.com>
2024-06-06 23:51:48 +0300

    Cards now are clickable

---
+++ lib/view/categoryview.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Categoryview extends StatelessWidget {
+  const Categoryview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox() ,
+    );
+  }
+}
--- lib/widgets/categorycard.dart
+++ lib/widgets/categorycard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:newsapp/modules/category_module.dart';
+import 'package:newsapp/view/categoryview.dart';
 
 class Categorycard extends StatelessWidget {
   const Categorycard({super.key, required this.ctgry});
@@ -8,31 +9,45 @@ class Categorycard extends StatelessWidget {
   final CategoryModule ctgry;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 250,
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15),
-            child: Image.asset(
-              ctgry.imageasseturl,
-              width: 250,
-              height: 150,
-              fit: BoxFit.cover,
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return Categoryview();
+            },
           ),
-          Text(
-            ctgry.categoryName,
-            style: const TextStyle(
-                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          )
-        ],
+        );
+      },
+      child: Container(
+        height: 150,
+        width: 250,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.asset(
+                ctgry.imageasseturl,
+                width: 250,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              ctgry.categoryName,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
